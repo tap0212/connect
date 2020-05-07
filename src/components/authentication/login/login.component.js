37f3f3cb9ddc4789fb82a3ee85c0d093a52acdaa
tap0212 <tapan.awasthi02@gmail.com>
2020-05-07 22:10:08 +0530

    save

--- src/components/authentication/login/login.component.js
+++ src/components/authentication/login/login.component.js
@@ -1,9 +1,128 @@
-import React from 'react'
+import React, {useState} from 'react'
+import VNav from '../../Navbar/verticalNav/vNav.component'
+import {Link,Redirect} from 'react-router-dom'
 import './login.styles.scss'
+import {signin, authenticate} from '../../../APICalls/auth'
+import { Grid } from '@material-ui/core'
+import { css } from "@emotion/core";
+import {Alert} from '@material-ui/lab'
+import {PropagateLoader} from "react-spinners";
+import {ReactComponent as Avatar } from '../../../assets/avatar.svg'
+import {ReactComponent as Connect} from '../../../assets/auth.svg'
 function Login() {
-    return (
-        <div className="login-container">
+    const override = css`
+     display: block;
+     margin-left:50%;
+    `;
 
+    const [values, setValues] = useState({
+        email:'',
+        password:'',
+        error:'',
+        loading:false,
+        didRedirect:false
+    })
+    const {email, password, error, loading,didRedirect} = values;
+
+
+
+    const handleChange = name => event => {
+        setValues({...values,error:false, [name]: event.target.value})
+    }
+
+
+    const onSubmit = event => {
+        event.preventDefault()
+        setValues({...values, error:'', loading:true});
+        signin({email,password})
+            .then(data => {
+                if(data.error){
+                    setValues({...values, error:data.error, loading:false})
+                }
+                else{
+                    authenticate(data, () => {
+                        setValues({
+                            ...values,
+                            didRedirect: true
+                        })
+                    })
+                }
+            }).catch(console.log("error in login"))
+    }
+
+    const performRedirect = () => {
+        if(didRedirect){
+                return <Redirect to='/'></Redirect> 
+        }
+    }
+
+    const Flash = () => {
+        if(error){
+            return  <Alert  severity="error"><span className="flash">{error}</span></Alert>
+        }
+        if(loading === true){
+            return <PropagateLoader
+            css={override}
+            size={22}
+            color={"#66FCF1"}
+            loading={loading}
+          />
+        }
+    }
+
+
+    return (
+        <div className="login">
+            <VNav/>
+            <div className="login-container">
+                <Grid container spacing={3}>
+                   
+                    <Grid item xs={12} sm={6}>
+                        <div className="login-box">
+                             <div className="tab">
+                                 <Link to='/login' className="link1">Login</Link>
+                                 <Link to='/signup' className="link2">Signup</Link>
+                             </div>
+                             <div className="avatar-container">
+                                 <Avatar className="avatar"/>
+                             </div>
+                             {Flash()}
+                            <form className="form">
+                                <div className="form-control">
+                                    <input 
+                                    onChange={handleChange("email")} 
+                                    value={email} 
+                                    type="email" 
+                                    required
+                                    placeholder="Enter Email"
+                                    className="input"/>
+                                </div>
+                                <div className="form-control">
+                                    <input 
+                                    onChange={handleChange("password")} 
+                                    value={password} 
+                                    required
+                                    type="password" 
+                                    placeholder="Enter Password" 
+                                    className="input"/>
+                               </div>
+                               
+                                <button 
+                                onClick={onSubmit}
+                                className="button">Log In
+                                </button>
+                            </form>
+                            {performRedirect()}
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <div className="connect-container">
+                            <Connect className="connect"/>
+                        </div>
+                    </Grid>
+                    
+                </Grid>
+                </div>
         </div>
     )
 }
--- src/components/authentication/signup/signup.component.js
+++ src/components/authentication/signup/signup.component.js
@@ -4,9 +4,16 @@ import {Link} from 'react-router-dom'
 import './signup.style.scss'
 import {signup} from '../../../APICalls/auth'
 import { Grid } from '@material-ui/core'
+import { css } from "@emotion/core";
+import {Alert} from '@material-ui/lab'
+import {PropagateLoader} from "react-spinners";
 import {ReactComponent as Avatar } from '../../../assets/avatar.svg'
 import {ReactComponent as Connect} from '../../../assets/connect.svg'
 function Signup() {
+    const override = css`
+     display: block;
+     margin-left:50%;
+    `;
 
     const [values, setValues] = useState({
         name: '', 
@@ -45,20 +52,39 @@ function Signup() {
         .catch(console.log("error in signup"))
     }
 
+    const Flash = () => {
+        if(error){
+            return  <Alert severity="error"><span className="flash">{error}</span></Alert>
+        }
+        if(success===true){
+            return  <Alert   severity="success"><span className="flash">Account Created <Link className="span" to="/login">Login Now</Link></span></Alert>
+        }
+        if(loading === true){
+            return <PropagateLoader
+            css={override}
+            size={25}
+            color={"#6C63FE"}
+            loading={loading}
+          />
+        }
+    }
+
     return (
         <div className="signup">
             <VNav/>
             <div className="signup-container">
                 <Grid container spacing={3}>
+                   
                     <Grid item xs={12} sm={6}>
                         <div className="signup-box">
                              <div className="tab">
                                  <Link to='/login' className="link1">Login</Link>
                                  <Link to='/signup' className="link2">Signup</Link>
                              </div>
-                             {/* <div className="avatar-container">
+                             <div className="avatar-container">
                                  <Avatar className="avatar"/>
-                             </div> */}
+                             </div>
+                             {Flash()}
                             <form className="form">
                                 <div className="form-control">
                                     <input
@@ -67,6 +93,7 @@ function Signup() {
                                     type="text"
                                     placeholder="Enter Name" 
                                     autoFocus 
+                                    required
                                     className="input"/>
                                 </div>
                                 <div className="form-control">
@@ -74,6 +101,7 @@ function Signup() {
                                     onChange={handleChange("email")} 
                                     value={email} 
                                     type="email" 
+                                    required
                                     placeholder="Enter Email"
                                     className="input"/>
                                 </div>
@@ -81,6 +109,7 @@ function Signup() {
                                     <input 
                                     onChange={handleChange("password")} 
                                     value={password} 
+                                    required
                                     type="password" 
                                     placeholder="Enter Password" 
                                     className="input"/>
@@ -97,12 +126,12 @@ function Signup() {
                             </form>
                         </div>
                     </Grid>
-
                     <Grid item xs={12} sm={6}>
                         <div className="connect-container">
                             <Connect className="connect"/>
                         </div>
                     </Grid>
+                    
                 </Grid>
                 </div>
         </div>
