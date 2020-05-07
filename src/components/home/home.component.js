62e637415f6583443614036ce376a968f090a432
tap0212 <tapan.awasthi02@gmail.com>
2020-05-07 22:47:31 +0530

    save

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid';
 import VNav from '../Navbar/verticalNav/vNav.component'
 import './home.styles.scss'
 function Home() {
@@ -6,7 +7,14 @@ function Home() {
         <div className="home-container">
             <VNav/>
             <div className="home-content-container">
-           
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={6}>
+
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        
+                    </Grid>
+                </Grid>
             </div>
 
         </div>
